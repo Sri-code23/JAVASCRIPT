79948d2cbfcdfeb7494c5cad0eccf71a2c34c6d4
Sri-code23 <sridharansri2312@gmail.com>
2025-03-15 13:42:24 +0530

    add ES6 modules with basic math functions and styling

---
+++ ES6 MODULES/math.js
@@ -0,0 +1,12 @@
+export const PI = 3.14159;
+
+function add(x,y){
+    return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+export {add, subtract};
+
---
+++ ES6 MODULES/script.js
@@ -0,0 +1,15 @@
+import {add, subtract, PI} from './math.js';
+
+console.log(add(1,2));
+console.log(subtract(8,2));
+console.log(PI);
+
+
+
+
+
+
+
+
+
+
